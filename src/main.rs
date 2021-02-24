d1b09b46e56fea805fca862b753c79383d33c90d
Luke Chu <lukewchu@outlook.com>
2021-02-24 22:48:46 +0000

    Fix

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <main>
-                { "Hello Yew!" }
+                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+                <h1>{ "Hello World!" }</h1>
+                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
             </main>
         }
     }
